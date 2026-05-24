527e81eb064262fd3ff3e196fc4016afa146774a
Surbhi Aglawe <aglawesurbhi@gmail.com>
2026-05-24 22:28:07 +0530

    added patient table

--- src/main/java/com/example/hospitalmanagement/controller/PatientController.java
+++ src/main/java/com/example/hospitalmanagement/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.example.hospitalmanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,10 @@ public class PatientController {
     }
 
     @GetMapping("/patients")
-    public String patientsPage() {
+    public String patientsPage(Model model) {
+
+        model.addAttribute("patients", repository.findAll());
+
         return "patients";
     }
 
